1b518519319db0f05e15c63324be940de987e2a6
yozi97 <lejliczijad@gmail.com>
2025-07-24 15:53:24 +0200

    jdbc

--- src/main/java/com/zijad/contactcalendar/controller/ContentController.java
+++ src/main/java/com/zijad/contactcalendar/controller/ContentController.java
@@ -2,6 +2,7 @@ package com.zijad.contactcalendar.controller;
 
 import com.zijad.contactcalendar.model.Content;
 import com.zijad.contactcalendar.repository.ContentCollectionRepository;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -33,7 +34,7 @@ public class ContentController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("")
-    public void create(@RequestBody Content content) {
+    public void create(@Valid @RequestBody Content content) {
         repository.save(content);
     }
 
--- src/main/java/com/zijad/contactcalendar/model/Content.java
+++ src/main/java/com/zijad/contactcalendar/model/Content.java
@@ -1,11 +1,15 @@
 package com.zijad.contactcalendar.model;
 
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+
 import java.time.LocalDateTime;
 
 public record Content(
 
     Integer id,
+    @NotBlank
     String title,
     String desc,
     Status status,
